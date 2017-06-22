3a0013b56f5bae4e2806c43c28dea6f72da88eea
haleyelder <haley.elder@live.com>
2017-06-21 21:52:07 -0700

    h style updates

--- content.php
+++ content.php
@@ -1,4 +1,31 @@
+<svg height="0" version="1.1" >
 
+    <filter id="turbulence-1">
+        <feTurbulence type="fractalNoise" baseFrequency="0.01" numOctaves="2" data-filterId="3"/>
+        <feDisplacementMap xChannelSelector="R" yChannelSelector="G" in="SourceGraphic" scale="5"/>
+    </filter>
+
+    <filter id="turbulence-2">
+        <feTurbulence type="fractalNoise" baseFrequency="0.015" numOctaves="2" data-filterId="3"/>
+        <feDisplacementMap xChannelSelector="R" yChannelSelector="G" in="SourceGraphic" scale="5"/>
+    </filter>
+
+    <filter id="turbulence-3">
+        <feTurbulence type="fractalNoise" baseFrequency="0.02" numOctaves="2" data-filterId="3"/>
+        <feDisplacementMap xChannelSelector="R" yChannelSelector="G" in="SourceGraphic" scale="5"/>
+    </filter>
+
+    <filter id="turbulence-4">
+        <feTurbulence type="fractalNoise" baseFrequency="0.025" numOctaves="2" data-filterId="3"/>
+        <feDisplacementMap xChannelSelector="R" yChannelSelector="G" in="SourceGraphic" scale="5"/>
+    </filter>
+
+    <filter id="turbulence-5">
+        <feTurbulence type="fractalNoise" baseFrequency="0.03" numOctaves="2" data-filterId="3"/>
+        <feDisplacementMap xChannelSelector="R" yChannelSelector="G" in="SourceGraphic" scale="5"/>
+    </filter>
+
+</svg>
   <!-- First Blog Post -->
 
   <div class="post">
